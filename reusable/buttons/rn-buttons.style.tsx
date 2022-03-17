b9ead0e7cef0e7c2e91f1c0423c388c37d1fe2c5
Igor Joshevski <gigo_j44@hotmail.com>
2022-03-17 17:50:38 +0100

    Set of buttons - defaults

---
+++ reusable/buttons/rn-buttons.style.tsx
@@ -0,0 +1,18 @@
+const buttonStyle:any = {
+	paddingTop: 4,
+	paddingBottom: 4,
+	paddingLeft: 4,
+	paddingRight: 4
+}
+
+const iconButtonStyle:any = {
+	flexDirection: 'row',
+	alignItems:'center',
+	...buttonStyle
+}
+
+const groupContainerStyle:any = {
+	flexDirection: 'row'
+}
+
+export {buttonStyle, iconButtonStyle, groupContainerStyle}
